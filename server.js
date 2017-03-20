ba88f682a523c122f5b9f465050db2c2472a28e6
Dtoyne <dylantoyne@gmail.com>
2017-03-20 14:40:28 -0400

    Express, BodyParser, Mongo

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is directory that contains the JavaScript source code.
+})
+
+app.post('/input', (req, res) => {
+  console.log(req.body);
+})
 
 app.listen(3000, function() {
   console.log('Listening on port 3000');
